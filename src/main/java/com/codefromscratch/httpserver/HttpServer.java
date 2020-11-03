4a2c3dae2b86a9908e3401424f2d13e71ab83ad8
BogdanBercea <bogdan.bercea98@yahoo.com>
2020-11-03 17:38:18 +0200

    WebServer v1.0 Added

---
+++ src/main/java/com/codefromscratch/httpserver/HttpServer.java
@@ -0,0 +1,42 @@
+package com.codefromscratch.httpserver;
+
+import com.codefromscratch.httpserver.config.Configuration;
+import com.codefromscratch.httpserver.config.ConfigurationManager;
+import com.codefromscratch.httpserver.core.ServerListenerThread;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+
+/**
+ *
+ * Driver for HTTP Server
+ *
+ */
+
+public class HttpServer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpServer.class);
+
+    public static void main(String[] args){
+
+        LOGGER.info("Server starting ...");
+
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+        Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
+
+        LOGGER.info("Using Port: " + conf.getPort());
+        LOGGER.info("Using Webroot: " + conf.getWebroot());
+
+        ServerListenerThread serverListenerThread = null;
+        try {
+            serverListenerThread = new ServerListenerThread(conf.getPort(), conf.getWebroot());
+            serverListenerThread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+            //TODO handle later
+        }
+
+    }
+}
---
+++ src/main/java/com/codefromscratch/httpserver/core/ServerListenerThread.java
@@ -0,0 +1,51 @@
+package com.codefromscratch.httpserver.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerListenerThread extends Thread{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpConnectionWorkerThread.class);
+
+    private int port;
+    private String webroot;
+    private ServerSocket serverSocket;
+
+    public ServerListenerThread(int port, String webroot) throws IOException {
+        this.port = port;
+        this.webroot = webroot;
+        this.serverSocket = new ServerSocket(this.port);
+    }
+
+    @Override
+    public void run() {
+        try {
+            while(serverSocket.isBound() && !serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept(); //At this point the program stops and wait to get a connection
+
+                LOGGER.info(" * Connection accepted: " + socket.getInetAddress());
+
+                HttpConnectionWorkerThread workerThread = new HttpConnectionWorkerThread(socket);
+                workerThread.start();
+            }
+            //serverSocket.close(); //TODO Handle close.
+
+        } catch (IOException e) {
+            LOGGER.error("Problem with setting socket", e);
+        } finally {
+            if (serverSocket != null){
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
